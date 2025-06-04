09064081572b13415202a759e5b8b50f544ae117
Sumeet Singh <sumeet605@gmail.com>
2025-06-04 17:43:33 -0400

    logging and font fixed frontend

--- next-frontend/src/app/layout.tsx
+++ next-frontend/src/app/layout.tsx
@@ -3,10 +3,17 @@ import {ThemeProvider} from "@/components/theme-provider"
 import React from "react";
 import AuthProvider from "@/components/auth-provider";
 
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+    subsets: ['latin'],
+    display: 'swap',
+});
+
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
     return (
         <html lang="en" suppressHydrationWarning>
-            <body>
+            <body className={inter.className}>
             <ThemeProvider
                 attribute="class"
                 defaultTheme="system"
