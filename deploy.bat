df3e775f19df34ccbc350c8df42ce7246c17d6fb
Sumeet Singh <sumeet605@gmail.com>
2025-06-04 18:23:25 -0400

    deploy removed

--- deploy.bat
+++
@@ -1,9 +0,0 @@
-docker tag ssinghnet/ai-playlist-api-gateway:latest 515964776068.dkr.ecr.us-east-1.amazonaws.com/ai-playlist/api-gateway:latest
-docker tag ssinghnet/ai-playlist-llm-service:latest 515964776068.dkr.ecr.us-east-1.amazonaws.com/ai-playlist/llm-service:latest
-docker tag ssinghnet/ai-playlist-spotify-service:latest 515964776068.dkr.ecr.us-east-1.amazonaws.com/ai-playlist/spotify-service:latest
-docker tag ssinghnet/ai-playlist-service-registry:latest 515964776068.dkr.ecr.us-east-1.amazonaws.com/ai-playlist/service-registry:latest
-
-docker push 515964776068.dkr.ecr.us-east-1.amazonaws.com/ai-playlist/api-gateway:latest
-docker push 515964776068.dkr.ecr.us-east-1.amazonaws.com/ai-playlist/llm-service:latest
-docker push 515964776068.dkr.ecr.us-east-1.amazonaws.com/ai-playlist/service-registry:latest
-docker push 515964776068.dkr.ecr.us-east-1.amazonaws.com/ai-playlist/spotify-service:latest
