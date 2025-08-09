09048714978f2df6ce9e560016003bb1e93f592f
Sumeet Singh <sumeet605@gmail.com>
2025-08-09 12:30:23 -0400

    added actuator health

---
+++ spotify-service/src/main/java/net/ssingh/spotifyservice/actuator/health/LLMClientHealthIndicator.java
@@ -0,0 +1,33 @@
+package net.ssingh.spotifyservice.actuator.health;
+
+import net.ssingh.spotifyservice.client.LLMClient;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LLMClientHealthIndicator implements HealthIndicator {
+
+    private final LLMClient llmClient;
+
+    public LLMClientHealthIndicator(LLMClient llmClient) {
+        this.llmClient = llmClient;
+    }
+
+    @Override
+    public Health health() {
+        try {
+            ResponseEntity<?> response = llmClient.healthCheck();
+            if (response.getStatusCode().is2xxSuccessful()) {
+                return Health.up().build();
+            } else {
+                return Health.down()
+                        .withDetail("status", response.getStatusCode())
+                        .build();
+            }
+        } catch (Exception e) {
+            return Health.down(e).build();
+        }
+    }
+}
--- spotify-service/src/main/java/net/ssingh/spotifyservice/client/LLMClient.java
+++ spotify-service/src/main/java/net/ssingh/spotifyservice/client/LLMClient.java
@@ -6,11 +6,15 @@ import net.ssingh.aiplaylist_common_files.model.entity.generic.Playlist;
 import net.ssingh.aiplaylist_common_files.model.entity.generic.Track;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name="LLM-SERVICE")
 public interface LLMClient {
+    @GetMapping("/actuator/health")
+    ResponseEntity<String> healthCheck();
+
     @PostMapping("playlist/generate")
     ResponseEntity<Playlist<Track<Artist>>> generatePlaylist(@RequestBody GeneratePlaylistRequest request);
 }
